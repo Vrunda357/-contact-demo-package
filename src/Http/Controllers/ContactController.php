a5500f9f60c4146f152b9e531a32df6ced2550d7
vrunda <vrundaptridhyatech@gmail.com>
2020-12-29 19:11:48 +0530

    first commit

---
+++ src/Http/Controllers/ContactController.php
@@ -0,0 +1,22 @@
+<?php
+namespace PriyaXYZ1\Contact\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use PriyaXYZ1\Contact\Models\Contact;
+use PriyaXYZ1\Contact\Mail\ContactMailable;
+use Mail;
+
+class ContactController extends Controller{
+
+	public function index()
+	{
+		return view('contact::contact');
+	}
+
+	public function send(Request $request){
+		//Mail::to(config('contact.send_email_to'))->send(new ContactMailable($request->message,$request->name));
+		Contact::create($request->all());
+        return redirect(route('contact'));
+	}
+}
